da87c8517bbdcc5f1c53844919f7ddeeee3eb1aa
sunny raju <mallepogusunny123@gmail.com>
2025-04-30 00:21:42 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -2,18 +2,11 @@ const express = require("express")
 
 const app = express()
 
-app.get("/user", (req, res) => {
+app.get("/user/:userId/:name/:password", (req, res) => {
+    console.log(req.params)
     res.send({firstName:"Sunny",lastName:"Raju"})
 })
-app.post("/user", (req, res) => {
-    res.send("Data successfully to the database")
-})
-app.delete("/user", (req, res) => {
-    res.send("Deleted successfully")
-})
-app.use("/test", (req, res, next) => {
-    res.send("Hello from the server")
-})
+
 
 app.listen(3000,()=>{
     console.log("servre started at port 3000")

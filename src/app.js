a7c7daa8562ee31beb7c1e10305c4fc0faa9ed8e
sunny raju <mallepogusunny123@gmail.com>
2025-04-30 00:03:06 +0530

    explored the get post delete request

--- src/app.js
+++ src/app.js
@@ -2,16 +2,17 @@ const express = require("express")
 
 const app = express()
 
-// app.use("/",(req,res)=>{
-//     res.send("Hello from dashboard")
-//     })
-
-    app.use("/hello",(req,res)=>{
-        res.send("Hello sunny mallepogu")
-        })
-
-app.use("/test",(req,res,next)=>{
-res.send("Hello from the server")
+app.get("/user", (req, res) => {
+    res.send({firstName:"Sunny",lastName:"Raju"})
+})
+app.post("/user", (req, res) => {
+    res.send("Data successfully to the database")
+})
+app.delete("/user", (req, res) => {
+    res.send("Deleted successfully")
+})
+app.use("/test", (req, res, next) => {
+    res.send("Hello from the server")
 })
 
 app.listen(3000,()=>{
